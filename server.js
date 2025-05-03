6a337089f364acca83feadefd8c2d03b31203c1e
Jordan-cybergeek <jordankafundawetou@gmail.com>
2025-05-03 17:45:09 +0200

    second test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-const bcrypt      = require ('bcrypt')
+const bcrypt      = require ('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -13,13 +13,9 @@ const someOtherPlaintextPassword = 'pass123';
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
-const hash=  bcrypt.hash(myPlaintextPassword, saltRounds ,(err, hash) => {
-    console. log (hash);
-    bcrypt.compare(someOtherPlaintextPassword , hash , (err ,res) =>{
-        console.log(res)
-    });
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
+  });
 
 
 
