2d7ab8567b1b34540339f5d48646f77606999121
Jordan-cybergeek <jordankafundawetou@gmail.com>
2025-05-03 18:27:23 +0200

    fix up on the spelling

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/', (req, res) => {
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash)=>{
-    console. log (hash);
-    bcrypt.comapre(myPlaintextPassword, hash, (err, res) =>{
+    console.log (hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) =>{
         console.log(res);
     });
 });
@@ -29,6 +29,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash)=>{
 //START_SYNC
 var hash = bcrypt.hashSync( myPlaintextPassword,saltRounds); 
 console.log(hash);
+
+var result =bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
